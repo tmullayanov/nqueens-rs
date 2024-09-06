2f89d5453946efedd8ea60dd4d94237158cdb44b
Timur Mullayanov <timur.r.mullayanov@gmail.com>
2024-09-07 00:00:54 +0300

    Make simple CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+use nqueens::*;
+use traversal::run;
+
+#[derive(Parser)]
+struct Cli {
+    #[arg(short('n'), long, value_parser=clap::value_parser!(u8).range(1..16))]
+    board_size: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    println!("Args: n={:?}", args.board_size);
+
+
+    let answer = run(args.board_size);
+    println!("Solution:");
+    println!("{:?}", answer);
 }
